ed30891a58f7ed3ab4fc5b79b3f0c13b9513bfb3
phbest777 <phbest777@163.com>
2023-05-06 17:27:08 +0800

    update

---
+++ src/main/java/org/ph/ssm/ZJJGWeb/bean/XzhouseParaAccinfo.java
@@ -0,0 +1,87 @@
+package org.ph.ssm.ZJJGWeb.bean;
+
+
+public class XzhouseParaAccinfo {
+
+  private String id;
+  private String orgid;
+  private String orgname;
+  private String jgaccount;
+  private String jgname;
+  private String contractno;
+  private String status;
+  private String datadate;
+
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+
+  public String getOrgid() {
+    return orgid;
+  }
+
+  public void setOrgid(String orgid) {
+    this.orgid = orgid;
+  }
+
+
+  public String getOrgname() {
+    return orgname;
+  }
+
+  public void setOrgname(String orgname) {
+    this.orgname = orgname;
+  }
+
+
+  public String getJgaccount() {
+    return jgaccount;
+  }
+
+  public void setJgaccount(String jgaccount) {
+    this.jgaccount = jgaccount;
+  }
+
+
+  public String getJgname() {
+    return jgname;
+  }
+
+  public void setJgname(String jgname) {
+    this.jgname = jgname;
+  }
+
+
+  public String getContractno() {
+    return contractno;
+  }
+
+  public void setContractno(String contractno) {
+    this.contractno = contractno;
+  }
+
+
+  public String getStatus() {
+    return status;
+  }
+
+  public void setStatus(String status) {
+    this.status = status;
+  }
+
+
+  public String getDatadate() {
+    return datadate;
+  }
+
+  public void setDatadate(String datadate) {
+    this.datadate = datadate;
+  }
+
+}
---
+++ src/main/java/org/ph/ssm/ZJJGWeb/bean/XzhouseParaOrginfo.java
@@ -0,0 +1,57 @@
+package org.ph.ssm.ZJJGWeb.bean;
+
+
+public class XzhouseParaOrginfo {
+
+  private String brhmBrNo;
+  private String brhmBrName;
+  private String brhmCentralBranch;
+  private String brhmCityCode;
+  private String brhmCnapsAfltPboc;
+
+
+  public String getBrhmBrNo() {
+    return brhmBrNo;
+  }
+
+  public void setBrhmBrNo(String brhmBrNo) {
+    this.brhmBrNo = brhmBrNo;
+  }
+
+
+  public String getBrhmBrName() {
+    return brhmBrName;
+  }
+
+  public void setBrhmBrName(String brhmBrName) {
+    this.brhmBrName = brhmBrName;
+  }
+
+
+  public String getBrhmCentralBranch() {
+    return brhmCentralBranch;
+  }
+
+  public void setBrhmCentralBranch(String brhmCentralBranch) {
+    this.brhmCentralBranch = brhmCentralBranch;
+  }
+
+
+  public String getBrhmCityCode() {
+    return brhmCityCode;
+  }
+
+  public void setBrhmCityCode(String brhmCityCode) {
+    this.brhmCityCode = brhmCityCode;
+  }
+
+
+  public String getBrhmCnapsAfltPboc() {
+    return brhmCnapsAfltPboc;
+  }
+
+  public void setBrhmCnapsAfltPboc(String brhmCnapsAfltPboc) {
+    this.brhmCnapsAfltPboc = brhmCnapsAfltPboc;
+  }
+
+}
